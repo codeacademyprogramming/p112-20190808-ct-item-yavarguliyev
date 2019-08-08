83260cc7ec079432b452a2f7b53f7f1c24114c59
yavarguliyev <yavarrg@code.edu.az>
2019-08-08 12:27:15 +0400

    added

--- js/script.js
+++ js/script.js
@@ -1,7 +1,13 @@
-$(window).on('scroll', function() {
-    if($(window).scrollTop()) {
-        $('nav').addClass('black');
-    } else {
-        $('nav').removeClass('black');
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        }
     }
-});
+})
